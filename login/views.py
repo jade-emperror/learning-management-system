9a523544b9fdca6f5b8263a9f8603022b328ba48
jade-emperror <maestrobharath@gmail.com>
2021-04-17 11:20:39 +0530

    login temroary fix done

--- login/views.py
+++ login/views.py
@@ -7,30 +7,39 @@ def login(request):
     return render(request,'login.html')
 
 def cklogin(request):
-    print(request.method)
-    if request.method=='POST':
+    if request.method == 'POST':
         uid=request.POST.get('uid').lower()
         pwd=request.POST.get('pwd')
-        if uid[2:]=='vh':
+        print(uid,uid[2:],uid[2:]=='vh')
+        if uid[:2]=='vh':
             data=Student.objects.filter(uid=uid).values('uid','password')
-            if(len(data!=0)):
+            print(data)    
+            if(len(data)!=0):
+                print(data)
                 data=data[0]
-                if(data['uid']==uid and data['pwd'] == pwd):
+                print(data)
+                if(data['uid']==uid and data['password'] == pwd):
                     return HttpResponse("loggedin")
                 else:
-                    return redirect(login,{'script':"<script>alert(\"wrong username or password\")</script>"})
-            else: 
-                return redirect(login,{'script':"<script>alert(\"wrong username or password\")</script>"})
-        elif uid[3:]=='hts':
+                    return render(request,'login.html',{'script':"<script>alert(\"wrong username or password\")</script>"})
+                    #return redirect(login,{'script':"<script>alert(\"wrong username or password\")</script>"})
+            else:
+                return render(request,'login.html',{'script':"<script>alert(\"wrong username or password\")</script>"}) 
+                #return redirect(login,{'script':"<script>alert(\"wrong username or password\")</script>"})
+        elif uid[:3]=='hts':
             data=Faculty.objects.filter(uid=uid).values('uid','password')
-            if(len(data!=0)):
+            if(len(data)!=0):
                 data=data[0]
-                if(data['uid']==uid and data['pwd'] == pwd):
+                if(data['uid']==uid and data['password'] == pwd):
                     return HttpResponse("loggedin")
                 else:
-                    return redirect(login,{'script':"<script>alert(\"wrong username or password\")</script>"})
+                    return render(request,'login.html',{'script':"<script>alert(\"wrong username or password\")</script>"})
+                    #return redirect(login,{'script':"<script>alert(\"wrong username or password\")</script>"})
             else: 
-                return redirect(login,{'script':"<script>alert(\"wrong username or password\")</script>"})
+                return render(request,'login.html',{'script':"<script>alert(\"wrong username or password\")</script>"})
+                #return redirect(login,{'script':"<script>alert(\"wrong username or password\")</script>"})
         else:
-            return redirect(login)
-    return HttpResponse("not post method find why") 
+            return render(request,'login.html',{'script':"<script>alert(\"wrong username or password\")</script>"})
+            #return redirect(login)
+    else:
+        return HttpResponse("fucked up for sure")
